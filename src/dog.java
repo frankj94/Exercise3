3c663220130961827f5d358e0ff0620e47d16a1d
Frank Walker <frank@franks-air.lan>
2022-03-01 16:45:56 -0500

    Initial commit

---
+++ src/dog.java
@@ -0,0 +1,8 @@
+public class dog {
+    public static void main(String[] args) {
+        int age = 3;
+        int dog_age = age*7;
+
+        System.out.println("Frank is " + age + " but is " + dog_age + " in dog years");
+    }
+}
